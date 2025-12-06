89060de7f757287f44acc84f8813f977f1427401
SHAJAHAN-S <126801300+SHAJAHAN-S@users.noreply.github.com>
2025-12-06 15:02:07 +0530

    feat:"Everything is changed"
     enter the commit message for your changes. Lines starting

--- newfile.tsx
+++ newfile.tsx
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 // Hii i did some changes here
+=======
+// Hii i did some changes here
+//  Now i did some chanes in the branch "NewBranch"
+>>>>>>> 72661c2 (feat:"Everything is changed")
